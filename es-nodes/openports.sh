0bd4ff0ccbe59ae3d1e918ce4f3a5079a31e0ff2
Manav Leslie <manav.leslie@stratifiedmedical.com>
2015-06-01 09:29:46 +0100

    added node data

---
+++ es-nodes/openports.sh
@@ -0,0 +1,4 @@
+for i in {9000..9999}; do
+VBoxManage modifyvm "boot2docker-vm" --natpf1 "tcp-port$i,tcp,,$i,,$i";
+VBoxManage modifyvm "boot2docker-vm" --natpf1 "udp-port$i,udp,,$i,,$i";
+done
---
+++ es-nodes/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/opt/elasticsearch-1.5.2/bin/elasticsearch -d
+#echo "Running this"
